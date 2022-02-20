e7bfb294e5c54a96199aeb94f6bf1ae0e1f180f4
Kirill <k.s.sinitsyn@gmail.com>
2022-02-20 16:31:33 +0300

    Стили для 404

--- project/src/components/screens/NotFoundScreen/NotFoundScreen.tsx
+++ project/src/components/screens/NotFoundScreen/NotFoundScreen.tsx
@@ -1,15 +1,18 @@
+import {Link} from 'react-router-dom';
+
 import Header from '../../Header/Header';
+import Footer from '../../Footer/Footer';
 
 import {MainPropsType} from '../../../types/MainPropsType';
-import Footer from '../../Footer/Footer';
 
 function NotFound(props: MainPropsType) {
   return (
     <div className="page page--gray page--main">
       <Header isLogin={props.isLogin}/>
-      <main className="page__not-found page__main--index">
-        <p>404</p>
-        <p>Страница не найдена</p>
+      <main className="page__not-found not-found page__main--index">
+        <p className="not-found__title">404</p>
+        <p className="not-found__subtitle">Страница не найдена</p>
+        <Link className="not-found__link" to={'/'}>На главную страницу</Link>
       </main>
       <Footer />
     </div>
